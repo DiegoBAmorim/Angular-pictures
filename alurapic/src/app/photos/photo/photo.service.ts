955c213ff98c05cf91790adfb60f256db5346f8f
DiegoBAmorim <diegobarbosamorim@gmail.com>
2023-02-02 20:16:21 -0300

    initial commit

---
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Photo } from "./photo";
+
+const API = "http://localhost:3000";
+
+@Injectable({
+  providedIn: 'root' // qualquer component terá esse cara disponivel na mesma instancia.
+})
+
+export class PhotoService{
+  constructor(private http: HttpClient){}
+
+  listFromUser(userName: string){
+    return this.http.get<Photo[]>(API + '/flavio/photos');
+  }
+}
